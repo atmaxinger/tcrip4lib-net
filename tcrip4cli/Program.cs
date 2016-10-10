5439ae14cb486f629e864d9f23e07fe465dc0e3b
Maximilian Irlinger <maxi6594@gmail.com>
2016-10-10 18:09:24 +0200

    Added a simple library and a simple CLI program

---
+++ tcrip4cli/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using tcrip4lib;
+
+
+namespace tcrip4cli
+{
+	class MainClass
+	{
+		public static void PrintStatus(TCRIP4 tcrip4)
+		{
+			var status = tcrip4.GetStatus().Result;
+
+			Console.WriteLine("Current Status:");
+			foreach (var rel in status.Relais)
+			{
+				Console.WriteLine($"\t{ rel.Name }: { rel.On }");
+			}
+			Console.WriteLine();
+			Console.WriteLine($"\tPot0: { status.Pot }");
+			Console.WriteLine();
+
+		}
+
+		public static void Main(string[] args)
+		{
+			TCRIP4 tcrip4 = new TCRIP4();
+			bool finished = false;
+
+			Console.WriteLine("Welcome to TCRIP 4 CLI");
+			Console.WriteLine("======================");
+			Console.WriteLine();
+
+			PrintStatus(tcrip4);
+
+			int numberOfRelais = tcrip4.GetNumberOfRelais().Result;
+
+			do
+			{
+				Console.Write("> ");
+				var command = Console.ReadLine().Trim().ToLower();
+
+				string[] split = command.Split(' ');
+
+				if (split[0].StartsWith("q"))
+				{
+					finished = true;
+				}
+				else if (split.Length == 2)
+				{
+					try
+					{
+						int relay = int.Parse(split[1]);
+
+						if (relay > 0 && relay <= numberOfRelais)
+						{
+
+							if (split[0].StartsWith("t"))
+							{
+								var now = tcrip4.ToggleRelay(relay).Result;
+							}
+							else if (split[0].StartsWith("on"))
+							{
+								var now = tcrip4.SwitchOn(relay).Result;
+							}
+							else if (split[0].StartsWith("off"))
+							{
+								var now = tcrip4.SwitchOff(relay).Result;
+							}
+						}
+						else
+						{
+							throw new FormatException();
+						}
+					}
+					catch (FormatException fe)
+					{
+						Console.WriteLine($"Not a valid relay number! (1 - { numberOfRelais })");
+					}
+				}
+				else
+				{
+					Console.WriteLine("Unknown Command");
+				}
+
+				PrintStatus(tcrip4);
+			} while (!finished);
+
+
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ tcrip4lib/TCRIP4.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using System.Xml;
+
+namespace tcrip4lib
+{
+	public class Relay
+	{
+		public int Number = 1;
+		public string Name { get; set; }
+		public bool On { get; set; }
+	}
+
+	public class Status
+	{
+		public List<Relay> Relais { get; set; } = new List<Relay>();
+		public bool Pot { get; } = false;
+	}
+
+	public class TCRIP4
+	{
+		public string Host { get; set; } = "192.168.0.201";
+		public int RelayCount { get; set; } = 4;
+
+		private string status_url = "/status.xml";
+		private string toggle_url = "/leds.cgi";
+
+
+		public TCRIP4()
+		{
+		}
+
+		public async Task<bool> SwitchOn(int number)
+		{
+			var status = await GetStatus();
+			if (status.Relais.Find((Relay obj) => obj.Number == number).On == false)
+			{
+				var on = await ToggleRelay(number);
+
+				return on;
+			}
+
+			return true;
+		}
+
+		public async Task<bool> SwitchOff(int number)
+		{
+			var status = await GetStatus();
+			if (status.Relais.Find((Relay obj) => obj.Number == number).On == true)
+			{
+				var on = await ToggleRelay(number);
+
+				return on;
+			}
+
+			return false;
+		}
+
+		public async Task<bool> ToggleRelay(int number)
+		{
+			HttpClient httpClient = new HttpClient();
+			httpClient.BaseAddress = new Uri("http://" + Host);
+
+			// WTF
+		 	await httpClient.PostAsync(toggle_url + "?led=" + number, new StringContent(""));
+
+			var status = await GetStatus();
+			return status.Relais.Find((Relay obj) => obj.Number == number).On;
+		}
+
+		public async Task<Status> GetStatus()
+		{
+			HttpClient httpClient = new HttpClient();
+			httpClient.BaseAddress = new Uri("http://" + Host);
+
+			Status status = new Status();
+
+			var ret = await httpClient.GetAsync(status_url);
+			var xmlstring = await ret.Content.ReadAsStringAsync();
+
+			XmlDocument doc = new XmlDocument();
+			doc.LoadXml(xmlstring);
+			var lednodes = doc.SelectNodes("/response/*[starts-with(name(), 'led')]");
+
+			int relayId = 1;
+			foreach (XmlNode node in lednodes)
+			{
+				Relay relay = new Relay();
+				relay.Number = relayId;
+				relay.Name = $"Relay { relayId }";
+				relay.On = (node.InnerText == "1") ? true : false;
+
+				status.Relais.Add(relay);
+
+				relayId++;
+			}
+
+			return status;
+		}
+
+		public async Task<int> GetNumberOfRelais()
+		{
+			var status = await GetStatus();
+
+			return status.Relais.Count;
+		}
+	}
+}
